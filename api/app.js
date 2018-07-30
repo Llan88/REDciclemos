1819ea607fee97c5c56cace5bd4bd9b63e26476e
Llan <llanca.dihue@gmail.com>
2018-07-30 19:02:02 -0300

    Merge branch 'desarrollo' of https://github.com/Llan88/REDciclemos into desarrollo

---
+++ api/app.js
@@ -0,0 +1,17 @@
+'user strict'
+
+var express = require('express'); //para manejar html
+var bodyParser = require('body-parser'); //covertir json en javaScript
+
+var app = express();
+
+//cargar rutas
+
+//cargar middlewares
+
+//cargar cors
+
+//rutas
+
+//cargar exportar
+module.exports = app;
